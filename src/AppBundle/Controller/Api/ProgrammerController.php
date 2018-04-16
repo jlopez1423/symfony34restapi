7a855ecb581b2452b3a31ceaa1dd3d12c6d5a1e9
Jose <jalopez1423@gmail.com>
2018-04-15 20:57:20 -0700

    added new programmer and programmer create form.

--- src/AppBundle/Controller/Api/ProgrammerController.php
+++ src/AppBundle/Controller/Api/ProgrammerController.php
@@ -2,19 +2,40 @@
 
 namespace AppBundle\Controller\Api;
 
+use AppBundle\Entity\Programmer;
+use AppBundle\Controller\BaseController;
+use AppBundle\Form\ProgrammerType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\VarDumper\VarDumper;
 
-class ProgrammerController extends Controller
+class ProgrammerController extends BaseController
 {
     /**
      * @Route("/api/programmers")
      * @Method("POST")
      */
-    public function newAction()
+    public function newAction(Request $request)
     {
-        return new Response('Let\'s do this!');
+//        $body = $request->getContent();
+        $data = json_decode($request->getContent(), true);
+
+        $programmer = new Programmer();
+        $form = $this->createForm(new ProgrammerType(), $programmer);
+        @$form->submit($data);
+
+
+        $programmer->setUser($this->findUserByUserName('weaverryan'));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($programmer);
+        $em->flush();
+
+        $response = new Response('It worked. Believe me I\'m an API!', 201);
+        $response->headers->set('Location', '/some/programmer/url');
+
+        return $response;
     }
 }
--- testing.php
+++ testing.php
@@ -6,9 +6,18 @@ $client = new \GuzzleHttp\Client([
     'base_url' => 'http://symfonyrestapi.l',
     'defaults' => [
         'exceptions' => false,
-    ]
+    ],
 ]);
 
-$response = $client->post('/api/programmers');
+$nickname = 'ObjectOrienter' . rand(0, 999);
+$data = [
+    'nickname'     => $nickname,
+    'avatarNumber' => 5,
+    'tagLine'      => 'a test dev!',
+];
+
+$response = $client->post('/api/programmers', [
+    'body' => json_encode($data),
+]);
 
 echo $response;
